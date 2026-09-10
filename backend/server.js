f276b721ad2b8401609138cd6a27005927de2b93
WaqarSiddiqui13 <waqarahmedsddqu2005@gmail.com>
2026-09-10 23:02:02 +0500

    change

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:5173','https://re-market-bano-qabil-5-0-spring-pro.vercel.app'
     credentials: true,
   }),
 );
